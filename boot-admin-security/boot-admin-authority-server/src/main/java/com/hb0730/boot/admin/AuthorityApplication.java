dea047ce12bbda743f71de908af452b93249048c
黄兵 <1278032416@qq.com>
2020-03-23 17:40:17 +0800

    init project

---
+++ boot-admin-security/boot-admin-authority-server/src/main/java/com/hb0730/boot/admin/AuthorityApplication.java
@@ -0,0 +1,18 @@
+package com.hb0730.boot.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * </P>
+ *
+ * @author bing_huang
+ * @since V1.0
+ */
+@SpringBootApplication
+public class AuthorityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorityApplication.class, args);
+    }
+}
