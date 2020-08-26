858229152dfc70078ce23a90973cb2115bb12066
黄兵 <1278032416@qq.com>
2020-08-26 08:42:48 +0800

    update

--- src/main/java/com/hb0730/boot/admin/BootAdminApplication.java
+++ src/main/java/com/hb0730/boot/admin/BootAdminApplication.java
@@ -1,11 +1,6 @@
 package com.hb0730.boot.admin;
 
-import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
-import com.alicp.jetcache.anno.config.EnableMethodCache;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 入口
@@ -13,11 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @author bing_huang
  * @since v1.0.0
  */
-@SpringBootApplication
-@EnableScheduling
-@EnableMethodCache(basePackages = "com.hb0730.boot.admin")
-@EnableCreateCacheAnnotation
-@EnableAsync
 public class BootAdminApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/hb0730/boot/admin/sms/service/impl/AliyunSmsServiceImplTest.java
+++
@@ -1,9 +0,0 @@
-package com.hb0730.boot.admin.sms.service.impl;
-
-import org.junit.Test;
-
-public class AliyunSmsServiceImplTest {
-    @Test
-    public void sendSms() {
-    }
-}
