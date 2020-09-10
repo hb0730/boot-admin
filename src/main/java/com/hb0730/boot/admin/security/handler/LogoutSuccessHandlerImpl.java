daf5f996b06ae8a034b9b080ba88ee77ae48076b
黄兵 <1278032416@qq.com>
2020-09-10 16:23:57 +0800

    update

--- src/main/java/com/hb0730/boot/admin/security/handler/LogoutSuccessHandlerImpl.java
+++ src/main/java/com/hb0730/boot/admin/security/handler/LogoutSuccessHandlerImpl.java
@@ -1,21 +1,23 @@
 package com.hb0730.boot.admin.security.handler;
 
+import com.hb0730.boot.admin.commons.enums.StatusEnum;
 import com.hb0730.boot.admin.domain.result.Result;
 import com.hb0730.boot.admin.domain.result.Results;
+import com.hb0730.boot.admin.manager.AsyncManager;
+import com.hb0730.boot.admin.manager.factory.AsyncFactory;
 import com.hb0730.boot.admin.security.model.User;
 import com.hb0730.boot.admin.token.ITokenService;
-import com.hb0730.commons.json.gson.GsonUtils;
+import com.hb0730.commons.json.utils.Jsons;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * 登出成功
@@ -28,17 +30,20 @@ import java.io.IOException;
 public class LogoutSuccessHandlerImpl implements LogoutSuccessHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(LogoutSuccessHandlerImpl.class);
     private final ITokenService tokenService;
+
+    @SneakyThrows
     @Override
-    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         LOGGER.debug("logout success <<<<<");
         User loginUser = tokenService.getLoginUser(request);
-        if (null!=loginUser){
+        if (null != loginUser) {
             tokenService.delLoginUser(request);
+            AsyncManager.me().execute(AsyncFactory.recordLoginLog(loginUser.getUsername(), StatusEnum.SUCCESS, "登出成功"));
         }
         Result<String> result = Results.resultSuccess("注销成功");
         response.setStatus(200);
         response.setContentType("application/json");
         response.setCharacterEncoding("utf-8");
-        response.getWriter().print(GsonUtils.objectToJson(result));
+        response.getWriter().print(Jsons.Utils.instance().objectToJson(result));
     }
 }
